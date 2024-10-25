cf39d01e6e353b178cc6273d5728d8af423db13b
Otávio Lima <otavio-lima-co@gmail.com>
2024-10-24 22:04:45 -0300

    picture and name

--- src/styles/container.js
+++ src/styles/container.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const ContainerStyles = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
 `
--- src/styles/portifoliocontainer.js
+++ src/styles/portifoliocontainer.js
@@ -1,9 +1,24 @@
 import styled from "styled-components";
 
 export const PortifolioContainerStyles = styled.div`
-width: 520px;
-height: 620px;
-background-color: #1F2326;
-border-radius: 22px;
-box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    width: 520px;
+    height: 620px;
+    background-color: #1F2326;
+    border-radius: 22px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+
+    img {
+        width: 150px;
+        margin-top: 20px;
+        border-radius: 50%;
+    }
+
+    h1 {
+        font-size: 32px;
+        color: #BABDBF;
+    }
+
 `
